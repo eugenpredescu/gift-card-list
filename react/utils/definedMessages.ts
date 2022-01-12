9b4ab3091df1fd9e41f0ab0d5c37b46091b2d6cf
Ana Maia <ana.baptistao@vtex.com.br>
2022-01-12 15:22:59 -0300

    messages

---
+++ react/utils/definedMessages.ts
@@ -0,0 +1,28 @@
+import { defineMessages } from 'react-intl'
+
+export const pageblock = defineMessages({
+  title: { id: 'giftcard.pageblock.title' },
+})
+
+export const dropdown = defineMessages({
+  placeholder: { id: 'giftcard.dropodown.placeholder' },
+  label: { id: 'giftcard.dropdown.label' },
+  description: { id: 'giftcard.dropdown.description' },
+})
+
+export const buttonSave = defineMessages({
+  save: { id: 'giftcard.button.save' },
+})
+
+export const alert = defineMessages({
+  save: { id: 'giftcard.data.save' },
+  error: { id: 'giftcard.data.save.errors' },
+})
+
+export const modalSave = defineMessages({
+  title: { id: 'giftcard.modal.save.title' },
+  cancel: { id: 'giftcard.modal.save.cancel' },
+  save: { id: 'giftcard.modal.save.save' },
+  text: { id: 'giftcard.modal.save.text' },
+  confirm: { id: 'giftcard.modal.save.confirm' },
+})

ca4176e7c016d8ce7e28d8d6feb7b62071d2f77d
Ana Maia <ana.baptistao@vtex.com.br>
2022-01-12 17:21:16 -0300

    removendo comentario

--- react/components/modalSave.tsx
+++ react/components/modalSave.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/restrict-plus-operands */
 import type { FC, SyntheticEvent } from 'react'
 import React, { useContext } from 'react'
 import { useIntl } from 'react-intl'

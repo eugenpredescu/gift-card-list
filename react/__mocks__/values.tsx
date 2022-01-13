c2cac6a6be1f6c691be517ad10d9f380c2a54f82
Ana Maia <ana.baptistao@vtex.com.br>
2022-01-13 10:21:03 -0300

    configs iniciais

---
+++ react/__mocks__/values.tsx
@@ -0,0 +1,11 @@
+export const values = {
+  account: 'account',
+  options: [{ label: 'account', value: 'account' }],
+  setAccount: jest.fn(),
+  save: jest.fn(),
+  clickSave: jest.fn(),
+  showAlert: 0,
+  modalSave: false,
+  handleCloseAlert: jest.fn(),
+  setModalSave: jest.fn(),
+}

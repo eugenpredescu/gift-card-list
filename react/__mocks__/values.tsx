46433677374345392341da2209bdf4aaaeef7c1d
Ana Maia <ana.baptistao@vtex.com.br>
2022-01-18 10:29:41 -0300

    add react-dom

--- react/__mocks__/values.tsx
+++ react/__mocks__/values.tsx
@@ -1,10 +1,12 @@
+import { ShowAlertOptions } from '../utils/showAlertOptions'
+
 export const values = {
   account: 'account',
   options: [{ label: 'account', value: 'account' }],
   setAccount: jest.fn(),
   save: jest.fn(),
   clickSave: jest.fn(),
-  showAlert: 0,
+  showAlert: ShowAlertOptions.notShow,
   modalSave: false,
   handleCloseAlert: jest.fn(),
   setModalSave: jest.fn(),

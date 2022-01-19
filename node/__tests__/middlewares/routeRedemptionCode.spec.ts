80bcc6510cb487c053662f873b1509f458f65fbd
Ana Maia <ana.baptistao@vtex.com.br>
2022-01-19 17:35:30 -0300

    test routeRedemptionCode

---
+++ node/__tests__/middlewares/routeRedemptionCode.spec.ts
@@ -0,0 +1,62 @@
+import co_body from 'co-body'
+
+import { routeRedemptionCode } from '../../middlewares/routeRedemptionCode'
+import { ERROR, HTTP_ERROR_MESSAGES, SUCESS } from '../../utils/constants'
+// eslint-disable-next-line jest/no-mocks-import
+import { ctx, ctxRouteError, ctxRouteSuccess } from '../../__mocks__/contexts'
+
+describe('Test routeRedemptionCode', () => {
+  it('testing if the return value is an error if I dont send an email', async () => {
+    jest
+      .spyOn(co_body, 'json')
+      .mockImplementation()
+      .mockResolvedValue({} as any)
+
+    await routeRedemptionCode(ctx)
+
+    expect(ctx.body.message).toBe(HTTP_ERROR_MESSAGES.missingEmail)
+    expect(ctx.status).toBe(ERROR)
+  })
+
+  it('testing if the return value is an error if I send invalid an email', async () => {
+    jest
+      .spyOn(co_body, 'json')
+      .mockImplementation()
+      .mockResolvedValue({
+        email: 'invalidEmail',
+      } as any)
+
+    await routeRedemptionCode(ctx)
+
+    expect(ctx.body.message).toBe(HTTP_ERROR_MESSAGES.invalidEmail)
+    expect(ctx.status).toBe(ERROR)
+  })
+
+  it('testing if the return is a success if have a redemptionCode', async () => {
+    jest
+      .spyOn(co_body, 'json')
+      .mockImplementation()
+      .mockResolvedValue({
+        email: 'email@email.com.br',
+      } as any)
+
+    await routeRedemptionCode(ctxRouteSuccess)
+
+    expect(ctxRouteSuccess.body).toStrictEqual('redemptionCode')
+    expect(ctxRouteSuccess.status).toBe(SUCESS)
+  })
+
+  it('testing if the return is an error if dont have a redemptionCode', async () => {
+    jest
+      .spyOn(co_body, 'json')
+      .mockImplementation()
+      .mockResolvedValue({
+        email: 'email@email.com.br',
+      } as any)
+
+    await routeRedemptionCode(ctxRouteError)
+
+    expect(ctxRouteError.body).toStrictEqual(HTTP_ERROR_MESSAGES.failed)
+    expect(ctxRouteError.status).toBe(ERROR)
+  })
+})

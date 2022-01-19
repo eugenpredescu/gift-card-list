aa8e5ca7dadb5e66708151acab20389f7302a9ab
Ana Maia <ana.baptistao@vtex.com.br>
2022-01-19 17:33:27 -0300

    mocks

---
+++ node/__mocks__/valuesGiftCardList.ts
@@ -0,0 +1,58 @@
+export const searchUndefined = {
+  data: [undefined],
+}
+
+export const searchValues = {
+  data: [
+    {
+      redemptionCode: 'redemptionCode',
+      giftCardId: 'giftCardId',
+      id: 'id',
+      quantityAlreadyInGiftCard: 0,
+      history: [
+        {
+          dateAndTime: '2022-01-07T19:37:16.2853567Z',
+          value: 2,
+        },
+      ],
+    },
+  ],
+}
+
+export const save = {
+  email: 'email@email.com.br',
+  giftCardId: 'giftCardId',
+  profileId: 'profileId',
+  quantityAlreadyInGiftCard: 2,
+  redemptionCode: 'redemptionCode',
+  history: {
+    dateAndTime: '2022-01-07T19:37:16.2853567Z',
+    value: 2,
+  },
+}
+
+export const update = {
+  email: 'email@email.com.br',
+  giftCardId: 'giftCardId',
+  profileId: 'profileId',
+  quantityAlreadyInGiftCard: 2,
+  redemptionCode: 'redemptionCode',
+  history: {
+    dateAndTime: '2022-01-07T19:37:16.2853567Z',
+    value: 2,
+  },
+}
+
+export const saveValues: SaveMasterdataValues = {
+  email: 'email@email.com.br',
+  giftCardId: 'giftCardId',
+  profileId: 'profileId',
+  quantityAlreadyInGiftCard: 2,
+  redemptionCode: 'redemptionCode',
+  history: [
+    {
+      dateAndTime: '2022-01-07T19:37:16.2853567Z',
+      value: 2,
+    },
+  ],
+}

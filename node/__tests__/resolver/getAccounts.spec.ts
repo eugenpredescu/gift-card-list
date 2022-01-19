b7b353a44eb4f68ea7602d9974b57c54df467921
Ana Maia <ana.baptistao@vtex.com.br>
2022-01-19 17:35:48 -0300

    test getAccount

---
+++ node/__tests__/resolver/getAccounts.spec.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { getAccounts } from '../../resolvers/getAccounts'
+
+describe('Test getAccounts', () => {
+  it('Test if getAccounts have a return', async () => {
+    // @ts-ignore
+    const ctx = {
+      clients: {
+        vlm: {
+          getAccounts: jest.fn().mockResolvedValue(['account1', 'account2']),
+        },
+      },
+    } as Context
+
+    const returnValue = await getAccounts('', '', ctx)
+
+    expect(returnValue).toStrictEqual(['account1', 'account2'])
+  })
+})

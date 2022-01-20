67d378198147d57e6fe5c325d12b8ea8592dfd1f
Ana Maia <ana.baptistao@vtex.com.br>
2022-01-20 17:07:06 -0300

    success

--- node/middlewares/routeHistory.ts
+++ node/middlewares/routeHistory.ts
@@ -1,6 +1,6 @@
 import { json } from 'co-body'
 
-import { ERROR, HTTP_ERROR_MESSAGES, SUCESS } from '../utils/constants'
+import { ERROR, HTTP_ERROR_MESSAGES, SUCCESS } from '../utils/constants'
 import { getInfoMasterdata } from '../utils/listMasterdata'
 import { validateEmail } from '../utils/validateEmail'
 
@@ -25,7 +25,7 @@ export async function routeHistory(ctx: Context) {
 
   if (masterdataInfo.data[0] !== undefined) {
     ctx.body = masterdataInfo.data[0].history
-    ctx.status = SUCESS
+    ctx.status = SUCCESS
   } else {
     ctx.body = HTTP_ERROR_MESSAGES.failed
     ctx.status = ERROR
